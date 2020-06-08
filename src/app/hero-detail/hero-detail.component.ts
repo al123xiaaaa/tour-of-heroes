aa1c51c115374d1c1765ff788c1c7b1bbd3b261a
IT--夏光为-- <xiagw@lingqidz.com>
2020-06-08 16:23:39 +0800

    Add in-app navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../Hero';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -9,9 +14,22 @@ import { Hero } from '../Hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,  // The ActivatedRoute holds information about the route to this instance of the HeroDetailComponent.
+    private heroService: HeroService,  // The HeroService gets hero data from the remote server and this component will use it to get the hero-to-display.
+    private location: Location,  // The location is an Angular service for interacting with the browser. You'll use it to navigate back to the view that navigated here.
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
