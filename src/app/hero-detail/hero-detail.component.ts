53b00a7ede7d9ce435e1c1061ccfb6a2dede0a02
IT--夏光为-- <xiagw@lingqidz.com>
2020-06-09 10:36:31 +0800

    update add delete functionality

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -29,7 +29,11 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
 
-  goBack() {
+  goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
   }
 }
